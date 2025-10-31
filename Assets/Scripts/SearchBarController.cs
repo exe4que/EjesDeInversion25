4b0b2501a4eb1ea979dfdc67ecaab9b629f3752a
Exequiel <exe4que@gmail.com>
2025-10-31 00:23:51 -0300

    Search bar dynamic size implemented

--- Assets/Scripts/SearchBarController.cs
+++ Assets/Scripts/SearchBarController.cs
@@ -17,6 +17,11 @@ namespace EjesDeInversion
         [SerializeField] private int _poolInitialSize = 20;
         [SerializeField] private int _elementsToProcessPerFrame = 50;
         
+        [Header("Dynamic Size Settings")]
+        [SerializeField] private float _verticalModeHorizontalPadding = 67f;
+        [SerializeField] private float _horizontalModeWidth = 956f;
+        [SerializeField] private Vector2 _horizontalModePosition = new Vector2(67f, -106f);
+        
         private string _searchText;
         private int _elementsProcessed = 0;
 
@@ -24,6 +29,8 @@ namespace EjesDeInversion
         private List<PointerController> _filteredPointerControllers = new();
         private List<SearchBarResultElementController> _resultElementsPool = new();
         private List<SearchBarResultElementController> _activeResultElements = new();
+        
+        private ScreenOrientation _currentOrientation = ScreenOrientation.Portrait;
 
         private void OnEnable()
         {
@@ -39,6 +46,31 @@ namespace EjesDeInversion
         {
             _allPointerControllers = PointersManager.GetAllActivePointers();
             InitializePool();
+            _currentOrientation = Screen.orientation;
+            AdjustDynamicSize();
+        }
+
+        private void AdjustDynamicSize()
+        {
+            RectTransform rectTransform = GetComponent<RectTransform>();
+            if (Screen.width < Screen.height) // Vertical mode
+            {
+                // anchor to top stretch
+                rectTransform.anchorMin = new Vector2(0f, 1f);
+                rectTransform.anchorMax = new Vector2(1f, 1f);
+                rectTransform.pivot = new Vector2(0.5f, 1f);
+                rectTransform.anchoredPosition = new Vector2(0f, _horizontalModePosition.y);
+                rectTransform.sizeDelta = new Vector2(-_verticalModeHorizontalPadding * 2, rectTransform.sizeDelta.y);
+            }
+            else // Horizontal mode
+            {
+                // anchor to top left corner
+                rectTransform.anchorMin = new Vector2(0f, 1f);
+                rectTransform.anchorMax = new Vector2(0f, 1f);
+                rectTransform.pivot = new Vector2(0f, 1f);
+                rectTransform.anchoredPosition = _horizontalModePosition;
+                rectTransform.sizeDelta = new Vector2(_horizontalModeWidth, rectTransform.sizeDelta.y);
+            }
         }
 
         private void InitializePool()
@@ -88,7 +120,21 @@ namespace EjesDeInversion
 
         private void Update()
         {
-            // Do search processing here
+            HandleSearch();
+            HandleDeviceOrientationChange();
+        }
+
+        private void HandleDeviceOrientationChange()
+        {
+            if (Screen.orientation != _currentOrientation)
+            {
+                _currentOrientation = Screen.orientation;
+                AdjustDynamicSize();
+            }
+        }
+
+        private void HandleSearch()
+        {
             if (!string.IsNullOrEmpty(_searchText) && _elementsProcessed < _allPointerControllers.Count)
             {
                 int processedThisFrame = 0;
